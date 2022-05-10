ace216b63046ba4178bf55124d78438e2b8f76a6
JWLK <utxtion@me.com>
2022-05-10 11:12:27 +0900

    Initial Commit

---
+++ RiddleRing/RiddleRingApp.swift
@@ -0,0 +1,17 @@
+//
+//  RiddleRingApp.swift
+//  RiddleRing
+//
+//  Created by JWLK on 2022/05/10.
+//
+
+import SwiftUI
+
+@main
+struct RiddleRingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
